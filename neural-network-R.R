ca2cef52a05945edf2d7e4f10ea292ac3e669410
parinpshah94 <parinpshah94@gmail.com>
2016-02-06 13:02:53 -0800

    neuralnets

---
+++ neural-network-R.R
@@ -0,0 +1,32 @@
+set.seed(500)
+#install.packages("MASS")
+install.packages("neuralnet")
+library(MASS)
+library(neuralnet)
+
+data <- Boston
+
+#Check for missing values
+apply(data,2,function(x) sum(is.na(x)))
+
+#Divide into test and train + fit a regression
+index <- sample(1:nrow(data),round(0.75*nrow(data)))
+train <- data[index,]
+test <- data[-index,]
+lm.fit <- glm(medv~., data=train)
+summary(lm.fit)
+pr.lm <- predict(lm.fit,test)
+MSE.lm <- sum((pr.lm - test$medv)^2)/nrow(test)
+
+#Normalizing the data
+maxs <- apply(data, 2, max) 
+mins <- apply(data, 2, min)
+scaled <- as.data.frame(scale(data, center = mins, scale = maxs - mins))
+train_ <- scaled[index,]
+test_ <- scaled[-index,]
+
+#Plotting the neural net
+n <- names(train_)
+f <- as.formula(paste("medv ~", paste(n[!n %in% "medv"], collapse = " + ")))
+nn <- neuralnet(f,data=train_,hidden=c(5,3),linear.output=T)
+plot(nn)
