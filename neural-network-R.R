3cde0a0d46ad1790f98c1fd188940f0955dc68fe
parinpshah94 <parinpshah94@gmail.com>
2016-02-06 15:13:26 -0800

    Update

--- neural-network-R.R
+++ neural-network-R.R
@@ -1,8 +1,11 @@
 set.seed(500)
 #install.packages("MASS")
-install.packages("neuralnet")
+#install.packages("neuralnet")
+#install.packages("boot")
+library(boot)
 library(MASS)
 library(neuralnet)
+library(plyr)
 
 data <- Boston
 
@@ -30,3 +33,69 @@ n <- names(train_)
 f <- as.formula(paste("medv ~", paste(n[!n %in% "medv"], collapse = " + ")))
 nn <- neuralnet(f,data=train_,hidden=c(5,3),linear.output=T)
 plot(nn)
+##The black lines show the connections between each layer and the weights on each connection while the blue lines show the bias term added in each step.
+
+#Predict medv values for the test dataset
+
+pr.nn <- compute(nn,test_[,1:13])
+
+pr.nn_ <- pr.nn$net.result*(max(data$medv)-min(data$medv))+min(data$medv)
+test.r <- (test_$medv)*(max(data$medv)-min(data$medv))+min(data$medv)
+
+MSE.nn <- sum((test.r - pr.nn_)^2)/nrow(test_)
+
+#Visualizing prediction
+
+par(mfrow=c(1,2))
+
+plot(test$medv,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='NN',pch=18,col='red', bty='n')
+
+plot(test$medv,pr.lm,col='blue',main='Real vs predicted lm',pch=18, cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='LM',pch=18,col='blue', bty='n', cex=.95)
+
+#Predictions made by the neural network are (in general) more concetrated around the line
+
+par(mfrow=c(1,1))
+plot(test$medv,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+points(test$medv,pr.lm,col='blue',pch=18,cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend=c('NN','LM'),pch=18,col=c('red','blue'))
+
+#Performing cross-validation
+set.seed(200)
+lm.fit <- glm(medv~.,data=data)
+cv.glm(data,lm.fit,K=10)$delta[1]
+
+set.seed(450)
+cv.error <- NULL
+k <- 10
+
+#Creating a progress-bar
+pbar <- create_progress_bar('text')
+pbar$init(k)
+
+for(i in 1:k){
+  index <- sample(1:nrow(data),round(0.9*nrow(data)))
+  train.cv <- scaled[index,]
+  test.cv <- scaled[-index,]
+  
+  nn <- neuralnet(f,data=train.cv,hidden=c(5,2),linear.output=T)
+  
+  pr.nn <- compute(nn,test.cv[,1:13])
+  pr.nn <- pr.nn$net.result*(max(data$medv)-min(data$medv))+min(data$medv)
+  
+  test.cv.r <- (test.cv$medv)*(max(data$medv)-min(data$medv))+min(data$medv)
+  
+  cv.error[i] <- sum((test.cv.r - pr.nn)^2)/nrow(test.cv)
+  
+  pbar$step()
+}
+
+mean(cv.error)
+
+boxplot(cv.error,xlab='MSE CV',col='cyan',
+        border='blue',names='CV error (MSE)',
+        main='CV error (MSE) for NN',horizontal=TRUE)
